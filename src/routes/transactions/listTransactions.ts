1b889f4cb707b38bf3516050989095d4730f33b9
Jimmy <jimmylanbcn@gmail.com>
2021-08-22 18:46:59 -0400

    Add listTransactions.ts

---
+++ src/routes/transactions/listTransactions.ts
@@ -0,0 +1,7 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2021-08-22
+ * Description:
+ *   Route to list transactions with pagination.
+ *   Currently listing from the most recent to the least recent.
+ */
