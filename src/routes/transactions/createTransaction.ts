e8384989b81d07254c913036bd8379d8a70a37d9
Jimmy <jimmylanbcn@gmail.com>
2021-08-21 23:58:18 -0400

    Add mongoose transaction logic

--- src/routes/transactions/createTransaction.ts
+++ src/routes/transactions/createTransaction.ts
@@ -11,6 +11,7 @@ import { ResBody } from "../../types";
 import { requireAuth, validateRequest } from "../../middlewares";
 import { body } from "express-validator";
 import * as mongoose from "mongoose";
+import { Transaction } from "../../models/Transaction";
 
 const router = Router();
 
@@ -24,11 +25,32 @@ router.post(
   validateRequest,
   async (req: Request, res: Response<ResBody>) => {
     const { title, pointsChange } = req.body;
+    const { id } = req.user!;
 
+    /*
+     * We should perform the following in this function:
+     * - (1) Create a new transaction for the current user, recording
+     *   the title of this transaction, if given, and points change.
+     * - (2) Update the number of points that the user has in the Users
+     *   document.
+     * These operations should be atomic. For example, if (2) fails, we
+     * should revert operation (1).
+     */
     const session = await mongoose.startSession();
+    await session.withTransaction(async () => {
+      // === Add transaction
+      const transaction = Transaction.build({
+        userId: id,
+        title,
+        pointsChange,
+      });
+      await transaction.save();
+      // === END Add transaction
 
-    await session.withTransaction(async () => {});
+      // === Add user points
 
+      // === END Add user points
+    });
     session.endSession();
 
     return res.json({
