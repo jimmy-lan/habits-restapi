1f53dd09515a2c66e122678e9be79b4a2295dc9d
Jimmy <jimmylanbcn@gmail.com>
2021-08-21 23:30:05 -0400

    Add body validation

--- src/routes/transactions/createTransaction.ts
+++ src/routes/transactions/createTransaction.ts
@@ -6,9 +6,21 @@
 
 import { Request, Response, Router } from "express";
 import { ResBody } from "../../types";
+import { requireAuth, validateRequest } from "../../middlewares";
+import { body } from "express-validator";
 
 const router = Router();
 
-router.post("/", (req: Request, res: Response<ResBody>) => {});
+router.post(
+  "/",
+  requireAuth,
+  [body("pointsChange").isInt().not().equals("0")],
+  validateRequest,
+  (req: Request, res: Response<ResBody>) => {
+    return res.json({
+      success: true,
+    });
+  }
+);
 
 export { router as createTransactionRoute };
