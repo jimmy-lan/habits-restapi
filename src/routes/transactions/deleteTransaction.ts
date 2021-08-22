306e08eb137267e045aff02c6a30ac3ea958d8ef
Jimmy <jimmylanbcn@gmail.com>
2021-08-22 12:32:39 -0400

    Add start session logic

--- src/routes/transactions/deleteTransaction.ts
+++ src/routes/transactions/deleteTransaction.ts
@@ -10,6 +10,7 @@ import { Router, Request, Response } from "express";
 import { requireAuth, validateRequest } from "../../middlewares";
 import { ResBody } from "../../types";
 import { query } from "express-validator";
+import mongoose from "mongoose";
 
 const router = Router();
 
@@ -18,17 +19,21 @@ router.delete(
   requireAuth,
   [query("transactionId").notEmpty().isMongoId()],
   validateRequest,
-  (req: Request, res: Response<ResBody>) => {
+  async (req: Request, res: Response<ResBody>) => {
     const { transactionId } = req.query;
     const user = req.user!;
 
     /*
      * We should perform the following in this function:
-     * - (1) Find the requested transaction for the current user.
+     * - (1) Find the requested transaction for the current user, and
+     *   set the transaction as deleted.
      * - (2) Update the number of points that the user has in the Users
      *   document after the transaction is reverted.
      * These operations should be atomic. For example, if (2) fails, we
      * should revert operation (1).
      */
+    const session = await mongoose.startSession();
+    session.withTransaction(async () => {});
+    session.endSession();
   }
 );
