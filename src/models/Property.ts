c0af7d76608faca357994f0af1180da28a019a46
Jimmy <jimmylanbcn@gmail.com>
2021-08-22 12:17:57 -0400

    Add timestamp interface

---
+++ src/models/Property.ts
@@ -0,0 +1,19 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2021-08-22
+ * Description:
+ *   Model describing properties of a user. Each user should
+ *   only have one property document entry.
+ */
+
+import { Document } from "mongoose";
+import { Timestamp } from "../types/Timestamp";
+
+interface PropertyProps {
+  /** ID of the user owning this property document. */
+  userId: string;
+  /** Number of points that the user has. */
+  points: number;
+}
+
+export type PropertyDocument = Document<PropertyProps> & Timestamp;
