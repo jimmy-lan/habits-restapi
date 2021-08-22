6d2f98068a57f2172e1386f625a3e14ba51645fe
Jimmy <jimmylanbcn@gmail.com>
2021-08-22 11:54:41 -0400

    Add comment to deleteTransaction.ts

--- src/routes/transactions/deleteTransaction.ts
+++ src/routes/transactions/deleteTransaction.ts
@@ -18,5 +18,17 @@ router.delete(
   requireAuth,
   [query("transactionId").notEmpty().isMongoId()],
   validateRequest,
-  (req: Request, res: Response<ResBody>) => {}
+  (req: Request, res: Response<ResBody>) => {
+    const { transactionId } = req.query;
+    const user = req.user!;
+
+    /*
+     * We should perform the following in this function:
+     * - (1) Find the requested transaction for the current user.
+     * - (2) Update the number of points that the user has in the Users
+     *   document after the transaction is reverted.
+     * These operations should be atomic. For example, if (2) fails, we
+     * should revert operation (1).
+     */
+  }
 );
