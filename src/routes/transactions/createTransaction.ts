d44654bb38fc4e983971d3552218734cb785ff3b
Jimmy <jimmylanbcn@gmail.com>
2021-08-21 23:23:59 -0400

    Initialize router

--- src/routes/transactions/createTransaction.ts
+++ src/routes/transactions/createTransaction.ts
@@ -3,3 +3,11 @@
  * Creation Date: 2021-08-21
  * Description: Route to create a transaction.
  */
+
+import { Request, Response, Router } from "express";
+
+const router = Router();
+
+router.post("/", (req: Request, res: Response) => {});
+
+export { router as createTransactionRoute };
