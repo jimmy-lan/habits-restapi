267a775091ce8531cb72bd244b21102b29ca47f3
Jimmy <jimmylanbcn@gmail.com>
2021-08-22 12:32:05 -0400

    Add schema definitions

--- src/models/Property.ts
+++ src/models/Property.ts
@@ -6,16 +6,56 @@
  *   only have one property document entry.
  */
 
-import { Document } from "mongoose";
+import { Document, Model, Schema } from "mongoose";
 import { Timestamp } from "../types/Timestamp";
+import mongoose from "mongoose";
 
 interface PropertyProps {
   /** ID of the user owning this property document. */
   userId: string;
-  /** Number of points that the user has. */
+  /** Number of points that the user has. Can be positive, negative, or 0. */
   points: number;
 }
 
 export type PropertyDocument = Document<PropertyProps> &
-  PropertyProps &
+  Required<PropertyProps> &
   Timestamp;
+
+const propertySchema = new Schema<PropertyDocument>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      unique: true,
+    },
+    points: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+      versionKey: false,
+    },
+  }
+);
+
+export interface PropertyModel extends Model<PropertyDocument> {
+  build(props: PropertyProps): PropertyDocument;
+}
+
+const build = (props: PropertyProps) => {
+  return new Property(props);
+};
+propertySchema.static("build", build);
+
+export const Property = mongoose.model<PropertyDocument, PropertyModel>(
+  "Property",
+  propertySchema
+);
