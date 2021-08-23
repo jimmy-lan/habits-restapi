aa6cfb0a50f5f368459794bee742d2d6ce98a6fe
Jimmy <jimmylanbcn@gmail.com>
2021-08-22 20:45:00 -0400

    Index routes

--- src/routes/transactions/index.ts
+++ src/routes/transactions/index.ts
@@ -2,13 +2,15 @@ import { Router } from "express";
 import { createTransactionRouter } from "./createTransaction";
 import { deleteTransactionRouter } from "./deleteTransaction";
 import { listTransactionsRouter } from "./listTransactions";
+import { modifyTransactionRouter } from "./modifyTransaction";
 
 const router = Router();
 
 router.use(
   createTransactionRouter,
   deleteTransactionRouter,
-  listTransactionsRouter
+  listTransactionsRouter,
+  modifyTransactionRouter
 );
 
 export { router as transactionsRouter };
