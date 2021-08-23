ae6e2429957e35cf1511c50cb8a238c458a4e1ff
Jimmy <jimmylanbcn@gmail.com>
2021-08-22 21:47:02 -0400

    Add userLimits.ts

---
+++ src/config/userLimits.ts
@@ -0,0 +1,11 @@
+/*
+ * Created by Jimmy Lan
+ * Creation Date: 2021-08-22
+ */
+
+/**
+ * Default limits that regular users have on resources.
+ */
+export const defaultUserLimits = {
+  maxTransactionCount: 1000,
+};
