6a4d30f2054789ae50143464a5ff68928c43ac6c
Jimmy <jimmylanbcn@gmail.com>
2021-08-24 22:24:58 -0400

    Add modify properties route

--- src/routes/properties/modifyProperties.ts
+++ src/routes/properties/modifyProperties.ts
@@ -2,3 +2,23 @@
 * Created by Jimmy Lan
 * Creation Date: 2021-08-25
 */
+
+import { Request, Response, Router } from "express";
+import { requireAuth, validateRequest } from "../../middlewares";
+import { body } from "express-validator";
+
+const router = Router();
+
+/**
+ * Route to adjust user points. This will set user points to a fixed number,
+ * and an adjustment transaction will be created automatically to reflect
+ * this.
+ */
+router.patch("/", requireAuth, [
+  body("points").isInt().not().isString()
+], validateRequest, async (req: Request, res: Response) => {
+  const {points} = req.body;
+  const user = req.user!;
+
+
+});
