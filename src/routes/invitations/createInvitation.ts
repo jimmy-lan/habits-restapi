cdaf06363ead3d448488cd2b994985558c39433f
Jimmy <jimmylanbcn@gmail.com>
2021-09-02 16:46:55 -0400

    Add post method

--- src/routes/invitations/createInvitation.ts
+++ src/routes/invitations/createInvitation.ts
@@ -1,3 +1,28 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { requireRoles, validateRequest } from "../../middlewares";
+import { UserRole } from "../../types";
+import { body } from "express-validator";
 
 const router = Router();
+
+router.post(
+  "/",
+  requireRoles([UserRole.admin]),
+  [
+    body("email").isEmail().normalizeEmail(),
+    body("sessionExpireAt")
+      .optional()
+      .isDate()
+      .toDate()
+      .custom((sessionExpireAt) => {
+        if (sessionExpireAt < new Date(new Date().getTime() + 5 * 1000)) {
+          throw new Error("Session too short.");
+        }
+      }),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    const { email, sessionExpireAt } = req.body;
+    console.log(sessionExpireAt);
+  }
+);
