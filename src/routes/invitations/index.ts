cf567d85864507ef0d79fe889c31a4eda076772f
Jimmy <jimmylanbcn@gmail.com>
2021-09-02 19:02:36 -0400

    Use activateInvitationRouter

--- src/routes/invitations/index.ts
+++ src/routes/invitations/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createInvitationRouter } from "./createInvitation";
+import { activateInvitationRouter } from "./activateInvitation";
 
 const router = Router();
 
-router.use(createInvitationRouter);
+router.use(createInvitationRouter, activateInvitationRouter);
 
 export { router as invitationsRouter };
