62b2c7dd5d8c96e36eb95f34d805c71ece837086
Jimmy <jimmylanbcn@gmail.com>
2021-09-02 18:55:49 -0400

    Implement create invitation route

--- src/routes/invitations/createInvitation.ts
+++ src/routes/invitations/createInvitation.ts
@@ -2,6 +2,8 @@ import { Request, Response, Router } from "express";
 import { requireRoles, validateRequest } from "../../middlewares";
 import { ResBody, UserRole } from "../../types";
 import { body } from "express-validator";
+import { Invitation } from "../../models";
+import { PasswordEncoder } from "../../services";
 
 const router = Router();
 
@@ -20,11 +22,19 @@ router.post(
       .toDate(),
   ],
   validateRequest,
-  (req: Request, res: Response<ResBody>) => {
+  async (req: Request, res: Response<ResBody>) => {
     const { email, sessionExpireAt } = req.body;
+
+    const invitation = await Invitation.build({
+      email,
+      code: PasswordEncoder.randomString(24),
+      testSessionExpireAt: sessionExpireAt,
+    });
+    await invitation.save();
 
     return res.status(201).json({
       success: true,
+      payload: invitation,
     });
   }
 );
