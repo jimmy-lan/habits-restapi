c3babb84665264b6192a41f362ee27a1dcb9d5ff
Jimmy <jimmylanbcn@gmail.com>
2021-09-12 12:46:02 -0400

    Update defaultQuota values

--- src/config/quota.ts
+++ src/config/quota.ts
@@ -5,7 +5,13 @@
 
 /**
  * Default limits that regular users have on resources.
+ * The max delete counts are valid for a period of time (i.e. the interval
+ * between two scheduled worker runs), as the num delete counts will be reset
+ * by the worker once scheduled check is completed.
  */
-export const defaultUserLimits = {
+export const defaultQuota = {
   maxTransactions: 1000,
+  maxDeletedTransactions: 1000,
+  maxProperties: 100,
+  maxDeletedProperties: 100,
 };
