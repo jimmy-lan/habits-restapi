90348634817e676363335589bbac7041e1afc4ca
Jimmy <jimmylanbcn@gmail.com>
2021-09-13 20:30:48 -0400

    Implement pagination

--- src/routes/properties/listProperties.ts
+++ src/routes/properties/listProperties.ts
@@ -6,18 +6,31 @@
 import { Request, Response, Router } from "express";
 import { ResBody } from "../../types";
 import { Property } from "../../models";
+import { validatePagination } from "../../middlewares";
 
 const router = Router();
 
-router.get("/", async (req: Request, res: Response<ResBody>) => {
-  const user = req.user!;
+router.get(
+  "/",
+  validatePagination,
+  async (req: Request, res: Response<ResBody>) => {
+    const { skip, limit } = req.query;
+    const user = req.user!;
 
-  const properties = await Property.find({ userId: user.id });
+    const findLimit: number = limit ? Number(limit) : 0;
+    const findSkip: number = skip ? Number(skip) : 0;
 
-  return res.json({
-    success: true,
-    payload: properties,
-  });
-});
+    const properties = await Property.find({ userId: user.id })
+      .sort({ createdAt: "desc" })
+      .skip(findSkip)
+      .limit(findLimit)
+      .exec();
+
+    return res.json({
+      success: true,
+      payload: properties,
+    });
+  }
+);
 
 export { router as listPropertiesRouter };
