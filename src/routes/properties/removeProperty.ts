4d6b0fb85d303e4c60def876a2524f39131456ab
Jimmy <jimmylanbcn@gmail.com>
2021-09-15 19:55:29 -0400

    Find property document

--- src/routes/properties/removeProperty.ts
+++ src/routes/properties/removeProperty.ts
@@ -2,6 +2,9 @@ import { Request, Response, Router } from "express";
 import { param } from "express-validator";
 import { validateRequest } from "../../middlewares";
 import mongoose from "mongoose";
+import { Property } from "../../models";
+import { notDeletedCondition } from "../../util";
+import { NotFoundError } from "../../errors";
 
 const router = Router();
 
@@ -12,6 +15,16 @@ router.delete(
   async (req: Request, res: Response) => {
     const { propertyId } = req.params;
     const user = req.user!;
+
+    // Check if property exists
+    const property = await Property.findOne({
+      userId: user.id,
+      _id: propertyId,
+      ...notDeletedCondition,
+    });
+    if (!property) {
+      throw new NotFoundError(`Property "${propertyId}" could not be found.`);
+    }
 
     // We need to remove the property together with all transactions
     // for this property.
