0ba172511541e2da0cbaff6d5d3bc0045a3d19dc
Jimmy <jimmylanbcn@gmail.com>
2021-09-14 21:11:16 -0400

    Add validation config

--- src/routes/properties/createProperty.ts
+++ src/routes/properties/createProperty.ts
@@ -4,9 +4,29 @@
 
 import { Request, Response, Router } from "express";
 import { validateRequest } from "../../middlewares";
+import { body } from "express-validator";
 
 const router = Router();
 
-router.post("/", [], validateRequest, (req: Request, res: Response) => {});
+router.post(
+  "/",
+  [
+    body("name")
+      .isString()
+      .isLength({ min: 1, max: 50 })
+      .withMessage(
+        "Property name must be a string of length between 1 and 50."
+      ),
+    body("description")
+      .isString()
+      .isLength({ min: 2, max: 100 })
+      .withMessage(
+        "Property description must be a string of length between 2 and 100."
+      ),
+    body("amountInStock").isNumeric(),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {}
+);
 
 export { router as createPropertyRouter };
