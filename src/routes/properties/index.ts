3bf42c02e48fe883912877328319bf8c6bc0c54d
Jimmy <jimmylanbcn@gmail.com>
2021-09-14 20:42:59 -0400

    Use createPropertyRouter

--- src/routes/properties/index.ts
+++ src/routes/properties/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { listPropertiesRouter } from "./listProperties";
 import { modifyPropertiesRouter } from "./updateProperties";
+import { createPropertyRouter } from "./createProperty";
 
 const router = Router();
 
-router.use(listPropertiesRouter, modifyPropertiesRouter);
+router.use(createPropertyRouter, listPropertiesRouter, modifyPropertiesRouter);
 
 export { router as propertiesRouter };
