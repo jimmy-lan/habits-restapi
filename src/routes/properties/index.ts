95108d032555ead4458d61532bf737b20b0b572f
Jimmy <jimmylanbcn@gmail.com>
2021-09-15 20:20:02 -0400

    Use delete property router

--- src/routes/properties/index.ts
+++ src/routes/properties/index.ts
@@ -2,9 +2,15 @@ import { Router } from "express";
 import { listPropertiesRouter } from "./listProperties";
 import { modifyPropertiesRouter } from "./updateProperties";
 import { createPropertyRouter } from "./createProperty";
+import { deletePropertyRouter } from "./removeProperty";
 
 const router = Router();
 
-router.use(createPropertyRouter, listPropertiesRouter, modifyPropertiesRouter);
+router.use(
+  createPropertyRouter,
+  deletePropertyRouter,
+  listPropertiesRouter,
+  modifyPropertiesRouter
+);
 
 export { router as propertiesRouter };
