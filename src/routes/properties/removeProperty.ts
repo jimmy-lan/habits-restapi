1dd0d1c7e7239fcf42ba8102ce6897534b671c58
Jimmy <jimmylanbcn@gmail.com>
2021-09-15 20:13:11 -0400

    Add transaction deletion logic

--- src/routes/properties/removeProperty.ts
+++ src/routes/properties/removeProperty.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from "express";
 import { param } from "express-validator";
 import { validateRequest } from "../../middlewares";
 import mongoose from "mongoose";
-import { Property } from "../../models";
+import { Property, Transaction } from "../../models";
 import { notDeletedCondition } from "../../util";
 import { NotFoundError } from "../../errors";
 
@@ -26,6 +26,9 @@ router.delete(
       throw new NotFoundError(`Property "${propertyId}" could not be found.`);
     }
 
+    // These values will be populated and returned
+    let numDeleted = 0;
+
     // We need to remove the property together with all transactions
     // for this property.
     const session = await mongoose.startSession();
@@ -34,12 +37,27 @@ router.delete(
       property.isDeleted = true;
       await property.save({ session });
       // === END Soft delete property
+
+      // === Soft delete all transactions with this property
+      const writeResult = await Transaction.updateMany(
+        {
+          userId: user.id,
+          property: propertyId,
+          ...notDeletedCondition,
+        },
+        { $set: { isDeleted: true } }
+      );
+      numDeleted = writeResult.nModified;
+      // === END Soft delete all transactions with this property
     });
     session.endSession();
 
     return res.status(202).json({
       success: true,
-      payload: property,
+      payload: {
+        property,
+        numDeleted,
+      },
     });
   }
 );
