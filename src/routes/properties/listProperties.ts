3a98127a5838729eeec4f35870803f5501830243
Jimmy <jimmylanbcn@gmail.com>
2021-09-16 23:07:20 -0400

    Remove validatePagination usage

--- src/routes/properties/listProperties.ts
+++ src/routes/properties/listProperties.ts
@@ -6,13 +6,15 @@
 import { Request, Response, Router } from "express";
 import { ResBody } from "../../types";
 import { Property } from "../../models";
-import { validatePagination } from "../../middlewares";
+import { validateRequest } from "../../middlewares";
+import { query } from "express-validator";
 
 const router = Router();
 
 router.get(
   "/",
-  validatePagination,
+  [query("fields").isArray()],
+  validateRequest,
   async (req: Request, res: Response<ResBody>) => {
     const { skip, limit } = req.query;
     const user = req.user!;
