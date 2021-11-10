1472c919ba0ecf0d3dd8e490ec81fa6927802493
Jimmy <jimmylanbcn@gmail.com>
2021-11-09 22:26:23 -0500

    Remove unnecessary comments

--- src/routes/transactions/createTransaction.ts
+++ src/routes/transactions/createTransaction.ts
@@ -40,7 +40,6 @@ router.post(
     // These values will be populated and returned
     let transaction: TransactionDocument;
 
-    // Find the property of interest
     const property = await Property.findOne({
       userId: user.id,
       _id: propertyId,
@@ -49,14 +48,6 @@ router.post(
       throw new NotFoundError("Could not locate this property.");
     }
 
-    /*
-     * We should perform the following in this function:
-     * - (1) Create a new transaction for the current user, recording
-     *   the information about this transaction.
-     * - (2) Update the amount of property that the user has.
-     * These operations should be atomic. For example, if (2) fails, we
-     * should revert operation (1).
-     */
     const session = await mongoose.startSession();
     await session.withTransaction(async () => {
       // === Add user points
